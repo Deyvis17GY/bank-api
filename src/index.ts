32d2fdafefbfa90dd7d79806af01210a4e862425
Deyvis Mariños <deyvisjaime17@gmail.com>
2023-10-31 03:10:34 -0500

    chore: add environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import dotenv from "dotenv";
-import app from "./app";
-import type { Port } from "./types";
+import dotenv from 'dotenv'
+dotenv.config()
+import app from './app'
+import type { Port } from './types'
 
-dotenv.config();
-
-const PORT: Port = process.env.PORT !== undefined ? Number(process.env.PORT) : 3000;
+const PORT: Port =
+  process.env.PORT !== undefined ? Number(process.env.PORT) : 3000
 
 const main = () => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-  });
-};
+    console.log(`Server is running on port: ${PORT}`)
+  })
+}
 
-main();
+main()
