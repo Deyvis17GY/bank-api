1957369822e54a63c4b4fcd719c9702596007133
Deyvis Mariños <deyvisjaime17@gmail.com>
2023-10-31 03:08:02 -0500

    feat: add utility password for login and register

---
+++ src/utility/password.ts
@@ -0,0 +1,46 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { UserModel } from '../models/UserModel'
+import { Response } from 'express'
+
+const { JWT_SECRET_KEY } = process.env
+
+export const GetSalt = async () => {
+  return await bcrypt.genSalt()
+}
+
+export const GetHashedPassword = async (password: string, salt: string) => {
+  return await bcrypt.hash(password, salt)
+}
+
+export const ValidatePassword = async ({
+  enteredPassword,
+  savePassword,
+  salt
+}: {
+  enteredPassword: string
+  savePassword: string
+  salt: string
+}) => {
+  return (await GetHashedPassword(enteredPassword, salt)) === savePassword
+}
+
+export const GetToken = (
+  res: Response,
+  { email, userId, lastName, firstName }: UserModel
+) => {
+  const userToken = jwt.sign(
+    { userId, email, lastName, firstName },
+    JWT_SECRET_KEY,
+    {
+      expiresIn: '12h'
+    }
+  )
+
+  res.cookie('jwt', userToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 12 * 60 * 60 * 1000
+  })
+}
